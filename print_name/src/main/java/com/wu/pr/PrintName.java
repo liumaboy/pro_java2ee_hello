bbd2131ca27fd701c66a653866c4161268f2a362
liumaboy <m18813091040@126.com>
2020-06-10 09:47:24 +0800

    add function_1_0

--- print_name/src/main/java/com/wu/pr/PrintName.java
+++ print_name/src/main/java/com/wu/pr/PrintName.java
@@ -2,6 +2,15 @@ package com.wu.pr;
 
 public class PrintName {
     public static void main(String[] args) {
+
         System.out.println("my name is wq");
+
+        System.out.println("now i add dep1.0's function");
+        function_1_0();
     }
+
+    public static void function_1_0(){
+        System.out.println("i am function 1.0");
+    }
+
 }
